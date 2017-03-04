987d0b5574e922376edaa054882127b7521e2628
e3xdd <ustce3ureka@gmail.com>
2017-03-05 00:19:45 +0800

    完成页面 编写基本游戏逻辑

--- main2048.js
+++ main2048.js
@@ -1,1 +1,22 @@
 // 游戏的主逻辑
+var board = new Array()
+var score = 0
+
+$(documen).ready(function(){
+    newgame()
+})
+
+var newgame = function() {
+    // 初始化棋盘
+    init()
+}
+
+var init = function() {
+    for (var i = 0; i < 4; i++) {
+        for (var j = 0; j < 4; i++) {
+            var gridCell = $("#grid-cell-" + i + "-" + j)
+            gridCell.css('top', getPosTop(i, j))
+            gridCell.css('left', getPosLeft(i, j))
+        }
+    }
+}
--- support2048.js
+++ support2048.js
@@ -1,1 +1,14 @@
-//底层支持逻辑 
+//底层支持逻辑
+var documentWidth = window.screen.availWidth
+var documentHeight = window.screen.availHeight
+var gridContainerWidth = documentWidth * 0.92
+var cellSideLength = documentWidth * 0.18
+var cellSpace = documentWidth * 0.04
+
+var getPosTop = function(i, j) {
+    return cellSpace + i * (cellSideLength + cellSpace)
+}
+
+var getPosLeft = function(i, j) {
+    return cellSpace + j * (cellSideLength + cellSpace)
+} 
