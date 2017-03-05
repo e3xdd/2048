48eb51e58505944c67beae843c304f80417b345c
e3xdd <ustce3ureka@gmail.com>
2017-03-05 15:56:02 +0800

    完成了部分动画逻辑

--- showanimation2048.js
+++ showanimation2048.js
@@ -1,1 +1,17 @@
-//动画效果逻辑 
+//动画效果逻辑
+var showNumberWithAnimation = function(i, j, randNumber) {
+    var numberCell = $(`#number-cell-${i}-${j}`)
+    numberCell.css('background-color', getNumberBackgroundColor(randNumber))
+    numberCell.css('color', getNumberColor(randNumber))
+    numberCell.css('font-size', getNumberSize(board[i][j]))
+    numberCell.text(randNumber)
+
+    // jquery 自带的动画函数
+    numberCell.animate({
+        width: cellSideLength,
+        height: cellSideLength,
+        top: getPosTop(i, j),
+        left: getPosLeft(i, j)
+    }, 50)
+    // console.log('showNumberWithAnimation');
+}
